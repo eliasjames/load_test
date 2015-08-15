c8927efa704b354b9b3ce20d1b9daced2fe347d2
Elias Carlston <elias@eliascarlston.com>
2015-08-15 16:10:16 -0500

    Functional improvements

--- test_spec.js
+++ test_spec.js
@@ -5,39 +5,50 @@ var repl = require("repl")
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 function loadTester() {
-  var server = 'https://njs-brexanshs.saplinglearning.me';
-
-  var user_name = 'txmath0001';
-  // placeholder for DB ID
-  var user_id;
-
-  var password = 'fasterthansixmill';
-  var assignment_id = 23;
-  var item_id = 2;
-
-  var locals = {};
-  var attempts_param = 5;
+  var locals = {
+    server: 'https://njs-brexanshs.saplinglearning.me',
+  
+    user_name: 'txmath0037',
+    user_id: 0,
+  
+    password: 'fasterthansixmill',
+    assignment_id: 23,
+    item_id: 2,
+  
+    attempts_param: 5
+  };
 
   function logStatement (opts) {
-    return console.log(opts.join(', ') + ', ' + Date.now());
+    var response = opts[opts.length - 1];
+    if (response) {
+      if (response.statusCode) {
+        opts[opts.length - 1] = response.statusCode;
+      }
+      if (response.statusCode !== 200) {
+        opts[opts.length] = response.body;
+      }
+    }
+    console.log(opts.join(', ') + ', ' + Date.now());
   }
 
   function getUserId(callback) {
-    var user_sql = ['SELECT id from cms.user where username = "' + user_name + '";'];
-    executeSQL(user_sql, function (body) {
-      user_id = JSON.parse(body).resultSets[0].id;
-      callback();
+    var user_sql = ['SELECT id from cms.user where username = "' + locals.user_name + '";'];
+    return executeSQL(user_sql, function (body) {
+      locals.user_id = JSON.parse(body).resultSets[0].id;
+      if (typeof callback === "function") {
+        callback();
+      }
     });
   }
 
   function clearAttempts(callback) {
 
     var sql = [
-      "DELETE FROM falcon.user_assignment_attempt                    WHERE assignment__id = " + assignment_id + " AND user__id = " + user_id + ";",
-      "DELETE FROM falcon.user_assignment_attempt_itemfulfillment    WHERE assignment__id = " + assignment_id + " AND user__id = " + user_id + ";",
-      "DELETE FROM falcon.user_item_attempt                          WHERE item__id =  " + item_id + " AND user__id = " + user_id + ";",
-      "DELETE FROM falcon.UIA_modchoiceset                           WHERE item__id =  " + item_id + " AND user__id = " + user_id + ";",
-      "DELETE FROM falcon.UIA_modnumeric                             WHERE item__id =  " + item_id + " AND user__id = " + user_id + ";",
+      "DELETE FROM falcon.user_assignment_attempt                    WHERE assignment__id = " + locals.assignment_id + " AND user__id = " + locals.user_id + ";",
+      "DELETE FROM falcon.user_assignment_attempt_itemfulfillment    WHERE assignment__id = " + locals.assignment_id + " AND user__id = " + locals.user_id + ";",
+      "DELETE FROM falcon.user_item_attempt                          WHERE item__id =  " + locals.item_id + " AND user__id = " + locals.user_id + ";",
+      "DELETE FROM falcon.UIA_modchoiceset                           WHERE item__id =  " + locals.item_id + " AND user__id = " + locals.user_id + ";",
+      "DELETE FROM falcon.UIA_modnumeric                             WHERE item__id =  " + locals.item_id + " AND user__id = " + locals.user_id + ";",
     ];
     executeSQL(sql, callback);
   }
@@ -46,14 +57,13 @@ function loadTester() {
     var j = 0;
     for (i=0; i<sql_array.length; i++) {
       frisbee.create('Execute SQL')
-        .post(server + '/sqlquery/', {
+        .post(locals.server + '/sqlquery/', {
           sql: sql_array[i]
         })
-        .auth(user_name, password)
+        .auth(locals.user_name, locals.password)
         .after(function (err, res, body) {
           j++;
-            console.log('\n\n\n', err);
-          logStatement(['End', 'Execute SQL', user_name, assignment_id, res.statusCode, sql_array[j]]);
+          logStatement(['End', 'Execute SQL', locals.user_name, locals.assignment_id, sql_array[j], res]);
           if (j >= sql_array.length) {
             if (typeof callback === "function") {
               callback(body);
@@ -65,35 +75,34 @@ function loadTester() {
   }
 
   function openAssignment() {
-    logStatement(['Begin', 'Open Assignment', user_name, assignment_id]);
+    logStatement(['Begin', 'Open Assignment', locals.user_name, locals.assignment_id, null]);
 
     frisbee.create('Open Assignment')
-        .post(server + '/v2/assignment/' + assignment_id)
-        .auth(user_name, password)
+        .post(locals.server + '/v2/assignment/' + locals.assignment_id)
+        .auth(locals.user_name, locals.password)
         .after(function (err, res, body) {
-          logStatement(['End', 'Open Assignment', user_name, assignment_id, res.statusCode]);
+          logStatement(['End', 'Open Assignment', locals.user_name, locals.assignment_id, res]);
           loadQuestion();
         })
       .toss();
   }
 
   function loadQuestion() {
-    logStatement(['Begin', 'Load Question', user_name, assignment_id]);
+    logStatement(['Begin', 'Load Question', locals.user_name, locals.assignment_id, null]);
 
     frisbee.create('Load Question')
-        .get(server + '/v2/student/item/' + item_id + '/' + assignment_id)
-        .auth(user_name, password)
+        .get(locals.server + '/v2/student/item/' + locals.item_id + '/' + locals.assignment_id)
+        .auth(locals.user_name, locals.password)
         .after(function (err, res, body) {
-          logStatement(['End', 'Load Question', user_name, assignment_id, res.statusCode]);
+          logStatement(['End', 'Load Question', locals.user_name, locals.assignment_id, res]);
           var body_json = JSON.parse(body);
-          var task_series = [];
 
+          var task_series = [];
           var incorrect_context_found = false;
           for (i=0; i<body_json.context.length && !incorrect_context_found; i++) {
             if (body_json.context[i].type == "incorrect") {
               locals.context = body_json.context[i];
-
-              for (j=0; j<attempts_param; j++) {
+              for (j=0; j<locals.attempts_param; j++) {
                 task_series.push(putAttempt);
                 task_series.push(postAttempt);
               }
@@ -108,13 +117,13 @@ function loadTester() {
   function makePayload() {
     var context_answer = locals.context.responseData[0];
     var payload_answer = {
-      "assignment_id": assignment_id,
-      "item_id": item_id,
+      "assignment_id": locals.assignment_id,
+      "item_id": locals.item_id,
       "is_correct": null,
       "gave_up": null,
       "context_id": locals.context.id,
       "raw_score": "0.0000",
-      "attempt_num": 1,
+      "attempt_num": 2,
       "answers": {}
     };
     payload_answer['answers'][context_answer.module_id] = {
@@ -128,16 +137,16 @@ function loadTester() {
 
   function putAttempt (callback) {
 
-    logStatement(['Begin', 'Put Attempt', user_name, assignment_id]);
+    logStatement(['Begin', 'Put Attempt', locals.user_name, locals.assignment_id, null]);
 
     frisbee.create('Put Attempt')
-      .put(server + '/v2/useritemattempt/', makePayload(), {json: true})
-      .auth(user_name, password)
+      .put(locals.server + '/v2/useritemattempt/', makePayload(), {json: true})
+      .auth(locals.user_name, locals.password)
       .after(function (err, res, body) {
-        logStatement(['End', 'Put Attempt', user_name, assignment_id, res.statusCode]);
+        logStatement(['End', 'Put Attempt', locals.user_name, locals.assignment_id, res]);
 
         var body_json = body;
-        if (typeof body == "string") {
+        if (res && res.statusCode === 200 && body === "string") {
           body_json = JSON.parse(body);
         }
 
@@ -147,16 +156,16 @@ function loadTester() {
   }
 
   function postAttempt (callback) {
-    logStatement(['Begin', 'Post Attempt', user_name, assignment_id]);
+    logStatement(['Begin', 'Post Attempt', locals.user_name, locals.assignment_id, null]);
 
     frisbee.create('Post Attempt')
-      .put(server + '/v2/useritemattempt/', makePayload(), {json: true})
-      .auth(user_name, password)
+      .put(locals.server + '/v2/useritemattempt/', makePayload(), {json: true})
+      .auth(locals.user_name, locals.password)
       .after(function (err, res, body) {
-          logStatement(['End', 'Post Attempt', user_name, assignment_id, res.statusCode]);
-          console.log('\n\n\n', body);
+          logStatement(['End', 'Post Attempt', locals.user_name, locals.assignment_id, res]);
+
           var body_json = body;
-          if (typeof body == "string") {
+          if (res && res.statusCode === 200 && body === "string") {
             body_json = JSON.parse(body);
           }
           callback(null, body_json);
@@ -165,6 +174,10 @@ function loadTester() {
 
   }
 
+  function runCycle () {
+    getUserId();
+    clearAttempts(openAssignment);
+  }
   return {
     clearAttempts: clearAttempts,
     getUserId: getUserId,
@@ -173,17 +186,24 @@ function loadTester() {
     openAssignment: openAssignment,
     putAttempt: putAttempt,
     postAttempt: postAttempt,
-    returnLocals: function () { return locals; }
+    returnLocals: function () { return locals; },
+    setLocals: function (key, val) { locals[key] = val; },
+    runCycle: runCycle
   }
 
 }
 
 var lt = loadTester();
-repl.start("> ").context.lt = lt;
+lt.openAssignment();
+//lt.getUserId(function () {
+//  lt.clearAttempts();
+//});
+//repl.start("> ").context.lt = lt;
+
 
 ////////////////////////////////////////////////////////////
 ////
-//// start with params: concurrecy, repeat-question, user, assignment_id
+//// start with params: concurrecy, repeat-question, user, locals.assignment_id
 //// start timer
 //// for each integer in concurrency
 ////   pick user
